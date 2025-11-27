10f9a4bc94b26b7ed289d2fc8a76541aeb31b56e
Adam Lopez <adamf04@gmail.com>
2025-11-27 01:55:00 -0500

    Adding a loop that runs twice...

--- bank.go
+++ bank.go
@@ -5,65 +5,68 @@ import "fmt"
 func main() {
 	var accountBalance float64 = 1000.0
 
-	fmt.Println("-----------------------------")
-	fmt.Println("| Welcome to Adam's Go Bank! |")
-	fmt.Println("-----------------------------")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Funds")
-	fmt.Println("3. Withdraw Funds")
-	fmt.Println("4. Exit")
+	// let us loop through
+	for i := 0; i < 2; i++ {
 
-	var choice int
-	fmt.Print("Your choice: ") // show a new line
-	fmt.Scan(&choice)          // pointer to choice var
-	// fmt.Println("-----------------------------")
-
-	// This would usually be put inline - but this is here
-	// just to show the that this is possible in Go
-	wantsCheckBalance := choice == 1
-
-	// Let's check what the user input is
-	if wantsCheckBalance {
-		fmt.Println("Your balance is", accountBalance)
 		fmt.Println("-----------------------------")
-	} else if choice == 2 {
+		fmt.Println("| Welcome to Adam's Go Bank! |")
 		fmt.Println("-----------------------------")
-		fmt.Print("Your Deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Funds")
+		fmt.Println("3. Withdraw Funds")
+		fmt.Println("4. Exit")
 
-		// check the deposit amount
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
+		var choice int
+		fmt.Print("Your choice: ") // show a new line
+		fmt.Scan(&choice)          // pointer to choice var
+
+		// This would usually be put inline - but this is here
+		// just to show the that this is possible in Go
+		wantsCheckBalance := choice == 1
+
+		// Let's check what the user input is
+		if wantsCheckBalance {
+			fmt.Println("Your balance is", accountBalance)
+		} else if choice == 2 {
+			fmt.Println("-----------------------------")
+			fmt.Print("Your Deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			// check the deposit amount
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				return
+			}
+
+			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+			fmt.Println("Balance Updated! New Amount:", accountBalance)
+			fmt.Println("-----------------------------")
+		} else if choice == 3 {
+			fmt.Println("-----------------------------")
+			fmt.Print("Your Withdrawl: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+
+			// withdraw functionality checks
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				return
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You cannot withdraw more than you have.")
+				return
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance Updated! New Amount:", accountBalance)
+		} else {
+			fmt.Println("-----------------------------")
+			fmt.Println("Goodbye!")
 		}
 
-		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
-		fmt.Println("Balance Updated! New Amount:", accountBalance)
-		fmt.Println("-----------------------------")
-	} else if choice == 3 {
-		fmt.Println("-----------------------------")
-		fmt.Print("Your Withdrawl: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-
-		// withdraw functionality checks
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
-		}
-
-		if withdrawAmount > accountBalance {
-			fmt.Println("Invalid amount. You cannot withdraw more than you have.")
-			return
-		}
-
-		accountBalance -= withdrawAmount
-		fmt.Println("Balance Updated! New Amount:", accountBalance)
-	} else {
-		fmt.Println("-----------------------------")
-		fmt.Println("Goodbye!")
 	}
 
 }
